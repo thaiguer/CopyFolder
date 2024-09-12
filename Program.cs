cfc3d8d522c5ab4b141c8199de5efe50301c8985
tgr <thaiguer.k@gmail.com>
2024-09-11 23:41:02 -0300

    Copying files async

--- Program.cs
+++ Program.cs
@@ -1,1 +1,49 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello!");
+
+string sourcePath = @"C:\proj";
+string targetPath = @"C:\projBKP";
+
+var sourceFiles = GetFilesAndSubFiles(sourcePath);
+Console.WriteLine($"{sourceFiles.Count} files found.");
+Console.WriteLine($"Copying files...");
+
+await CopyFilesAsync(sourceFiles, targetPath);
+Console.WriteLine("End of the copy.");
+Console.ReadKey();
+
+async Task CopyFilesAsync(List<string> sourceFiles, string targetPath)
+{
+    var tasks = new List<Task>();
+    foreach (var i in sourceFiles)
+    {
+        FileInfo fileInfo = new(i);
+        tasks.Add(CopyFileAsync(i, $@"{targetPath}\{fileInfo.Name}"));
+    }
+    await Task.WhenAll(tasks);
+}
+
+async Task CopyFileAsync(string sourceFile, string targetFile)
+{
+    try
+    {
+        using (Stream source = File.Open(sourceFile, FileMode.Open, FileAccess.ReadWrite))
+        {
+            using (Stream destination = File.Create(targetFile))
+            {
+                await source.CopyToAsync(destination);
+            }
+        }
+        Console.WriteLine($"{targetFile}: copied.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"{targetFile}: {ex.Message}.");
+    }
+}
+
+List<string> GetFilesAndSubFiles(string path)
+{
+    var files = new List<string>();
+    files.AddRange(Directory.GetFiles(path, "*", SearchOption.AllDirectories).ToList());    
+    return files;
+}
